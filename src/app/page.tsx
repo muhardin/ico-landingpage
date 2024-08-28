2409e97cafa1759cf5cc72976357b74f52703e19
Muhardin H. <muhardin@gmail.com>
2024-08-28 20:04:59 +0700

    ww

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import HomeThree from "@/component/homes/home-three";
 import Wrapper from "@/layouts/Wrapper";
 
 export const metadata = {
-  title: "Bico - ICO & Crypto Landing React Next js Template",
+  title: "Bico - ICO & Crypto With Real Project",
 };
 const index = () => {
   return (
